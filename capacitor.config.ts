90dc9ba2684bf6aeeec7f90919b9fcf1c5f1519d
skhacker105 <skhacker105@gmail.com>
2025-01-28 19:06:37 +0530

    completed capacitor addition and apk generation.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'small-games',
+  webDir: 'dist/small-games/browser'
+};
+
+export default config;
